a349448f860b8fac47fe8e61b75213385d573866
oksana-feshchenko <foksanv@gmail.com>
2023-04-03 11:46:03 +0300

    create admin.py

--- list/admin.py
+++ list/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from list.models import Task, Tag
+
+admin.site.unregister(Group)
+
+
+@admin.register(Task)
+class Task(admin.ModelAdmin):
+    list_display = ["content", "created_at", "deadline", "completed"]
+    list_filter = ["content"]
+    search_fields = ["title"]
+
+
+@admin.register(Tag)
+class Tag(admin.ModelAdmin):
+    list_display = ["name"]
+    search_fields = ["name"]
+
