8b86b28b3ebb1fecf9177cf01ae1c1529d01dcaf
oksana-feshchenko <foksanv@gmail.com>
2023-04-03 12:10:05 +0300

    create :create, update, delete view for tag

--- list/views.py
+++ list/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 
-from list.models import Task
+from list.models import Task, Tag
 
 
 def index(request):
@@ -10,3 +12,24 @@ def index(request):
     }
 
     return render(request, "list/index.html", context=context)
+
+
+class TagListView(generic.ListView):
+    model = Tag
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("taxi:manufacturer-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("taxi:manufacturer-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("taxi:manufacturer-list")
